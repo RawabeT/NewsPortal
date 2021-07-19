18ced58ebd091a34178d0d4f1197ff960e3a708c
RawabeT <rawabe44u@gmail.com>
2021-07-19 23:08:53 +0300

    Authenticate

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,38 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Models\Article;
 
-Route::get('/',[Controller::class,'index']);
-Route::get('/category',[Controller::class,'category']);
-Route::get('/about',[Controller::class,'about']);
-Route::get('/contact',[Controller::class,'contact']);
-Route::get('/detail', [Controller::class,'detail']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-
-Route::get('/login', function () {
-    return view('login');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/news',[NewsController::class,'newss']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-
-
-Route::group(['prefix'=>'admin'], function(){
-    Route::get('/', [DashboardController::class, 'admin']);
-    Route::get('/read', [DashboardController::class, 'read']);
-    Route::get('/create', [DashboardController::class, 'create']);
-    Route::get('/edit', [DashboardController::class, 'edit']);
-    Route::post('/create-post', [DashboardController::class, 'createArticle']);
-
-});
-
-Route::resource('/operations', DashboardController::class);
-
-// Route::get('/admin', [DashboardController::class, 'admin']);
-// Route::get('/read', [DashboardController::class, 'read']);
-// Route::get('/create', [DashboardController::class, 'create']);
-// Route::get('/edit', [DashboardController::class, 'edit']);
+require __DIR__.'/auth.php';
